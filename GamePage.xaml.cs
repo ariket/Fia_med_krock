c76a79b5ed68d6282ab2c28bdd05eb649b5b5af7
Ari Ketola <ariketola@hotmail.com>
2024-02-23 08:24:17 +0100

    Add project files.

---
+++ GamePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Capture;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+
+
+
+namespace Fia_med_krock
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void RollDice_Click(object sender, RoutedEventArgs e)
+        {
+          
+                Thread.Sleep(1000);
+                PlayBoard.Children.Remove(RedCar1);
+                PlayBoard.Children.Add(RedCar1);
+                Grid.SetRow(RedCar1, 2);
+                Grid.SetColumn(RedCar1,5);
+          
+        }
+    }
+}
