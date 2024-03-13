d7cbfd4249aeca35306e121d7e586d7d5555d5f7
MStameus <morris.stameus@hotmail.se>
2024-03-13 23:19:11 +0100

    brutit ut den stora animationen för bilarna till en egen file och klass för att göra mer läsbart och även lagt movehelper klassen i en egen fil

---
+++ GameLogic/MainAnimations.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Fia_med_krock.MainPage;
+
+namespace Fia_med_krock.GameLogic
+{
+    internal class MainAnimations
+    {
+        //FIXME: Gör så att funktionen inte är världens största if-else träd.
+        //bounce får sitt värde från goForward variabeln i Animatecarasync funktionen.
+        public static void PickAnimation(Windows.UI.Xaml.Shapes.Rectangle carToMove, int columnNum, int car_steps, GameState active_player, bool moveForward)
+        {
+            //Väljer olika animationer beroende på vilken spelare som kör.
+            if (active_player == GameState.PlayerRed)
+            {
+                if (moveForward == false)
+                {
+                    if (car_steps > 30)
+                        MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                    //Om man står på rutan bredvid mål och slår 6 så måste man ändra riktning igen.
+                    //Animationen blir rätt skum, men den hamnar på rätt plats iallafall.
+                    else
+                    {
+                        MoveHelper.AnimateCarDown(carToMove, columnNum);
+                    }
+                }
+                else if (car_steps <= 3)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 6)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 8)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 11)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 14)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 16)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 19)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 22)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 24)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 27)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 30)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 31)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps >= 32)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+            }
+            if (active_player == GameState.PlayerBlue)
+            {
+                if (moveForward == false)
+                {
+                    if (car_steps > 30)
+                        MoveHelper.AnimateCarUp(carToMove, columnNum);
+                    //Om man står på rutan bredvid mål och slår 6 så måste man ändra riktning igen.
+                    else
+                    {
+                        MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                    }
+                }
+                else if (car_steps <= 3)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 6)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 8)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 11)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 14)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 16)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 19)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 22)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 24)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 27)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 30)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 31)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps >= 32)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+            }
+            if (active_player == GameState.PlayerGreen)
+            {
+                if (moveForward == false)
+                {
+                    if (car_steps > 30)
+                        MoveHelper.AnimateCarRight(carToMove, columnNum);
+                    //Om man står på rutan bredvid mål och slår 6 så måste man ändra riktning igen.
+                    else
+                    {
+                        MoveHelper.AnimateCarUp(carToMove, columnNum);
+                    }
+                }
+                else if (car_steps <= 3)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 6)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 8)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 11)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 14)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 16)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 19)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 22)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 24)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 27)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 30)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 31)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps >= 32)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+            }
+            if (active_player == GameState.PlayerYellow)
+            {
+                if (moveForward == false)
+                {
+                    if (car_steps > 30)
+                        MoveHelper.AnimateCarDown(carToMove, columnNum);
+                    //Om man står på rutan bredvid mål och slår 6 så måste man ändra riktning igen.
+                    else
+                    {
+                        MoveHelper.AnimateCarRight(carToMove, columnNum);
+                    }
+                }
+                else if (car_steps <= 3)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 6)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 8)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 11)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 14)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+                else if (car_steps <= 16)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 19)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 22)
+                {
+                    MoveHelper.AnimateCarRight(carToMove, columnNum);
+                }
+                else if (car_steps <= 24)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 27)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps <= 30)
+                {
+                    MoveHelper.AnimateCarDown(carToMove, columnNum);
+                }
+                else if (car_steps <= 31)
+                {
+                    MoveHelper.AnimateCarLeft(carToMove, columnNum);
+                }
+                else if (car_steps >= 32)
+                {
+                    MoveHelper.AnimateCarUp(carToMove, columnNum);
+                }
+            }
+        }
+    }
+}
---
+++ GameLogic/MoveHelper.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml;
+
+namespace Fia_med_krock.GameLogic
+{
+    /// <summary>
+    /// MoveHelper class
+    /// Takes care of moving the game pieces
+    /// </summary>
+    public static class MoveHelper
+    {
+        //Tar samma argument som 'movecar' funktionen
+        public static void AnimateCarRight(Windows.UI.Xaml.Shapes.Rectangle carToMove, int columnNum)
+        {
+            //Animering för bilar
+            //Skapar objekten som behövs för att göra animeringen
+            carToMove.RenderTransform = new CompositeTransform();
+            Storyboard oStoryboard = new Storyboard();
+            DoubleAnimation oDoubleAnimation = new DoubleAnimation();
+
+            //Värden som bestämmer vart elementet ska flyttas och hur snabbt.
+            int startPosition = columnNum - 50;
+            oDoubleAnimation.From = startPosition;
+            oDoubleAnimation.To = startPosition + 50;
+            oDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            //oDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(durationTime*200));
+
+            //Applicerar animationen
+            Storyboard.SetTarget(oDoubleAnimation, carToMove);
+            //(CompositeTransform.TranslateY) flyttar vertikalt istället!
+            Storyboard.SetTargetProperty(oDoubleAnimation, "(UIElement.RenderTransform).(CompositeTransform.TranslateX)");
+
+            //Lägger till animationen i storyboard objektet
+            oStoryboard.Children.Add(oDoubleAnimation);
+            //Kör animering
+            oStoryboard.Begin();
+        }
+
+        //Samma som de andra
+        public static void AnimateCarLeft(Windows.UI.Xaml.Shapes.Rectangle carToMove, int columnNum)
+        {
+            carToMove.RenderTransform = new CompositeTransform();
+            Storyboard oStoryboard = new Storyboard();
+            DoubleAnimation oDoubleAnimation = new DoubleAnimation();
+            int startPosition = columnNum + 50;
+            oDoubleAnimation.From = startPosition;
+            oDoubleAnimation.To = startPosition - 50;
+            oDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            Storyboard.SetTarget(oDoubleAnimation, carToMove);
+            Storyboard.SetTargetProperty(oDoubleAnimation, "(UIElement.RenderTransform).(CompositeTransform.TranslateX)");
+            oStoryboard.Children.Add(oDoubleAnimation);
+            oStoryboard.Begin();
+        }
+
+        //Samma som de andra
+        public static void AnimateCarDown(Windows.UI.Xaml.Shapes.Rectangle carToMove, int columnNum)
+        {
+            carToMove.RenderTransform = new CompositeTransform();
+            Storyboard oStoryboard = new Storyboard();
+            DoubleAnimation oDoubleAnimation = new DoubleAnimation();
+            int startPosition = columnNum - 50;
+            oDoubleAnimation.From = startPosition;
+            oDoubleAnimation.To = startPosition + 50;
+            oDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            Storyboard.SetTarget(oDoubleAnimation, carToMove);
+            Storyboard.SetTargetProperty(oDoubleAnimation, "(UIElement.RenderTransform).(CompositeTransform.TranslateY)");
+            oStoryboard.Children.Add(oDoubleAnimation);
+            oStoryboard.Begin();
+        }
+
+        //Samma som de andra
+        public static void AnimateCarUp(Windows.UI.Xaml.Shapes.Rectangle carToMove, int columnNum)
+        {
+            carToMove.RenderTransform = new CompositeTransform();
+            Storyboard oStoryboard = new Storyboard();
+            DoubleAnimation oDoubleAnimation = new DoubleAnimation();
+            int startPosition = columnNum + 50;
+            oDoubleAnimation.From = startPosition;
+            oDoubleAnimation.To = startPosition - 50;
+            oDoubleAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(100));
+            Storyboard.SetTarget(oDoubleAnimation, carToMove);
+            Storyboard.SetTargetProperty(oDoubleAnimation, "(UIElement.RenderTransform).(CompositeTransform.TranslateY)");
+            oStoryboard.Children.Add(oDoubleAnimation);
+            oStoryboard.Begin();
+        }
+
+        public static void MoveCar(Windows.UI.Xaml.Shapes.Rectangle carToMove, Grid playBoard, int columnNum, int rowNum)
+        {
+            var parent = (Panel)carToMove.Parent;
+
+            if (parent != null)
+            {
+                parent.Children.Remove(carToMove);
+            }
+            playBoard.Children.Add(carToMove);
+            Grid.SetRow(carToMove, rowNum);
+            Grid.SetColumn(carToMove, columnNum);
+        }
+    }
+}
